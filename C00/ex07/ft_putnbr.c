0ac8a0002cbd5ed60bf1521e5beebe57da01462f
Tiffany Manolis <tiffany.mano@gmail.com>
2021-02-07 18:40:55 +0100

    MAJ ftputnbr

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -19,7 +19,7 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int nb)
 {
-	if (nb > 0 && nb <= 9)
+	if (nb >= 0 && nb < 10)
 	{
 		ft_putchar(nb + '0');
 	}
@@ -32,17 +32,8 @@ void	ft_putnbr(int nb)
 
 	else if (nb > 0)
 	{
-		ft_putchar(nb / 10 + '0');
-		ft_putchar(nb % 10 + '0');
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
 	}
 
-}
-
-int		main(void)
-{
-	int a;
-	a = 42;
-	
-	ft_putnbr(a);
-	return (0);
 }
