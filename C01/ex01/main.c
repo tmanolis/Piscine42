9a6be4c910d7296fb8613076b398e1c82e6220d1
Tiffany MANOLIS <tmanolis@e3r4p12.42.fr>
2021-02-08 15:58:51 +0100

    ex01

---
+++ C01/ex01/main.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmanolis <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/08 13:58:20 by tmanolis          #+#    #+#             */
+/*   Updated: 2021/02/08 15:50:44 by tmanolis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void ft_ultimate_ft(int *********nbr);
+
+int	main(void)
+{
+	int a;
+	int *nbr1;
+	int **nbr2;
+	int ***nbr3;
+	int ****nbr4;
+	int *****nbr5;
+	int ******nbr6;
+	int *******nbr7;
+	int ********nbr8;
+	int *********nbr9;
+
+	a = 10;
+	nbr1 = &a;
+	nbr2 = &nbr1;
+	nbr3 = &nbr2;
+	nbr4 = &nbr3;
+	nbr5 = &nbr4;
+	nbr6 = &nbr5;
+	nbr7 = &nbr6;
+	nbr8 = &nbr7;
+	nbr9 = &nbr8;
+
+
+	
+	ft_ultimate_ft(nbr9);
+	printf("%d", a);
+	return (0);
+
+}
