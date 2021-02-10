044419cfd432f983ca0829b989d951e1da409b78
Tiffany MANOLIS <tmanolis@e3r4p12.42.fr>
2021-02-10 17:22:51 +0100

    ex07

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -6,11 +6,22 @@
 /*   By: tmanolis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 13:06:54 by tmanolis          #+#    #+#             */
-/*   Updated: 2021/02/10 14:36:55 by tmanolis         ###   ########.fr       */
+/*   Updated: 2021/02/10 17:21:59 by tmanolis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_rev_int_tab(int *tab, int size)
 {
-	 tab 
+	int i;
+	int cache;
+
+	i = 0;
+	while (tab[i] < size / 2)
+	{
+		cache = tab[i];
+		tab[i] = tab[size] - 1 - i;
+		tab[size] - 1 - i = cache;
+		i++;
+	}
+
 }
--- C01/ex07/main.c
+++ C01/ex07/main.c
@@ -6,19 +6,37 @@
 /*   By: tmanolis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 11:43:13 by tmanolis          #+#    #+#             */
-/*   Updated: 2021/02/10 14:37:01 by tmanolis         ###   ########.fr       */
+/*   Updated: 2021/02/10 17:21:57 by tmanolis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 void	ft_rev_int_tab(int *tab, int size);
 
 int		main(void)
 {
 
-	int tab[a];
-	int a;
+	int size;
 
-	a = 5;
-	ft_rev_int_tab(tab, a);
+	size  = 5;
+
+	int tab[size] = {1, 2, 3, 4, 5};
+	int p;
+
+	p = 0;
+	while (p < size)
+	{
+		printf("%d", tab[p]);
+		p++;
+	}
+	ft_rev_int_tab(tab, size);
+	printf("\n");
+	p = 0;
+	while (p < size)
+	{
+		printf("%d", tab[p]);
+		p++;
+	}
 	return (0);
 }
