e249d6978750e2981ffed76bd612d0dae3d88202
Tiffany MANOLIS <tmanolis@e3r4p12.42.fr>
2021-02-11 17:22:47 +0100

    ex01

--- C02/ex01/main.c
+++ C02/ex01/main.c
@@ -1,32 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmanolis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/11 10:13:44 by tmanolis          #+#    #+#             */
-/*   Updated: 2021/02/11 17:21:08 by tmanolis         ###   ########.fr       */
+/*   Created: 2021/02/11 10:08:33 by tmanolis          #+#    #+#             */
+/*   Updated: 2021/02/11 17:22:26 by tmanolis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strncpy(char *dest, char *src, unsigned int n)
+#include <stdio.h>
+
+char *ft_strncpy(char *dest, char *src, unsigned int n);
+
+int		main(void)
 {
+	char dest[] = "Hel";
+	char src[] = "OKAY";
+	unsigned int n;
 
-	//utilise strlen
-	unsigned int i;
+	n = 5;
 
-	i = 0;
-
-	while (i < n && src[i] != '\0') //utilise strlen pour lui dire ou s'arreter
-		{
-			dest[i] = src[i];
-			i++;
-		}
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
-	return(dest);
+	printf("%s\n%s\n\n", dest, src);
+	ft_strncpy(dest, src, n);
+	printf("%s", dest);
+	return (0);
 }
