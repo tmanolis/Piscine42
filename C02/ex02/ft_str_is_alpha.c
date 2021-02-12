fb25224cf23760bc16b3294a5aa8889bcfd5ebee
Tiffany Manolis <tiffany.mano@gmail.com>
2021-02-12 15:20:48 +0100

    prouty

---
+++ C02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,24 @@
+int	ft_str_is_alpha(char *str)
+{
+	int i;
+	
+	i = 0;	
+	while (str[i] != '\0')
+	{
+		if ((str[i] > 0 && str[i] < 65) || (str[i] > 90 && str[i] , 97) 
+		|| (str[i] > 122))
+		{
+			return (0);
+			
+		}
+		else if ((str[i] = 0)
+		{
+			return (1);
+		}
+		else	
+			i++;
+		
+	}
+		return (1);
+	}
+
