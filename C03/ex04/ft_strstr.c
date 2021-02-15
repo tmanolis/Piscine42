68da0bad717b75b8a1a81f89a03f806a8c05d00f
Tiffany Manolis <tiffany.mano@gmail.com>
2021-02-15 21:07:45 +0100

    ex04

---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,32 @@
+int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strstr(char *str,char *to_find)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i + j] == to_find[j])
+		{
+			j++;
+		}
+		i++;
+	}
+	if (j != ft_strlen(to_find))
+		return (0);
+	else
+		return (str + i);
+}
