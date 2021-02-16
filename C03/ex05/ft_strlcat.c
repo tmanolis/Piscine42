06ce50ae72abeadb585e54b08bb2033eeec4c9fe
Tiffany MANOLIS <tmanolis@e3r4p12.42.fr>
2021-02-16 13:02:54 +0100

    ex05 sa mere

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -1,15 +1,38 @@
-unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
-{
-	unsigned int i;
-	unsigned int j;
-	
-	i = 0;
-	j = 0;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmanolis <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/16 13:01:16 by tmanolis          #+#    #+#             */
+/*   Updated: 2021/02/16 13:02:17 by tmanolis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-	while (dest[i] != '\0')
+int				ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
 	{
 		i++;
 	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int cache;
+
+	i = ft_strlen(dest);
+	j = 0;
+	cache = ft_strlen(dest);
+	if (dest[0] == '\0' || size == 0)
+		return (ft_strlen(src));
 	while (src[j] != '\0' && j < size)
 	{
 		dest[i] = src[j];
@@ -22,5 +45,8 @@ unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
 	{
 		i++;
 	}
-	return (i + size);
+	if (size < cache)
+		return (cache - (cache - size) + i);
+	else
+		return (cache + i);
 }
--- C03/ex05/main.c
+++ C03/ex05/main.c
@@ -2,20 +2,22 @@
 #include <string.h>
 
 unsigned int ft_strlcat(char *dest, char *src, unsigned int size);
+int ft_strlen(char *str);
 
 int	main(void)
 {
-	char dest[20] = "Coucou";
-	char src[] = "Toi Moi";
+	char dest[30] = "Toi";
+	char src[] = "Hello";
+	
+	char dest1[30] = "Toi";
+	char src2[] = "Hello";
 	unsigned int size;
 
-	size = 7;
+	size = 0;
 	
 	printf("%s\n%s\n", dest, src);
-	ft_strlcat(dest, src, size);
-	printf("%s\n", dest);
-	printf("%lu\n", strlcat(dest, src, size));
-	printf("%u\n", ft_strlcat(dest, src, size));
+	printf("%s %lu\n", "la vraie fonction :", strlcat(dest1, src2, size));
+	printf("%s %u\n", "ma fonction :", ft_strlcat(dest, src, size));
 
 	return (0);
 }
