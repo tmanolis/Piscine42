e2a1f041801981063abe75bfad434730dcdce750
Tiffany MANOLIS <tmanolis@e3r4p12.42.fr>
2021-02-16 11:31:34 +0100

    ex04

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -1,32 +1,35 @@
-int	ft_strlen(char *str)
-{
-	int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmanolis <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/16 10:30:36 by tmanolis          #+#    #+#             */
+/*   Updated: 2021/02/16 11:31:18 by tmanolis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-char	*ft_strstr(char *str,char *to_find)
+char	*ft_strstr(char *str, char *to_find)
 {
 	int i;
 	int j;
 
 	i = 0;
+	if (to_find[0] == '\0')
+		return (str);
 	while (str[i] != '\0')
 	{
 		j = 0;
 		while (str[i + j] == to_find[j])
 		{
+			if (to_find[j + 1] == '\0')
+			{
+				return (str + i);
+			}
 			j++;
 		}
 		i++;
 	}
-	if (j != ft_strlen(to_find))
-		return (0);
-	else
-		return (str + i);
+	return (0);
 }
