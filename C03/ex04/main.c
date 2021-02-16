a4e1cdc72d59a7c0ae176ba919e740355b4f3208
Tiffany MANOLIS <tmanolis@e3r4p12.42.fr>
2021-02-16 13:04:31 +0100

    maj

--- C03/ex04/main.c
+++ C03/ex04/main.c
@@ -2,15 +2,13 @@
 #include <string.h>
 
 char	*ft_strstr(char *str,char *to_find);
-int	ft_strlen(char *str);
 
 int	main(void)
 {
 	char str[] = "Salut les nazes";
-	char to_find[] = "le";
+	char to_find[] = "12";
 
-	//printf("%s\n", strstr(str, to_find));
+	printf("%s\n", strstr(str, to_find));
 	printf("%s\n", ft_strstr(str, to_find));
-	printf("%d", ft_strlen(to_find));
 	return (0);
 }
