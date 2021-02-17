c6e217458b43e4bacca39d45942832811cbd5a94
Tiffany MANOLIS <tmanolis@e3r4p12.42.fr>
2021-02-17 17:29:12 +0100

    c'est good

--- C06/ex02/ft_rev_params.c
+++ C06/ex02/ft_rev_params.c
@@ -6,7 +6,7 @@
 /*   By: tmanolis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 10:07:59 by tmanolis          #+#    #+#             */
-/*   Updated: 2021/02/17 17:27:31 by tmanolis         ###   ########.fr       */
+/*   Updated: 2021/02/17 17:28:38 by tmanolis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int		main(int argc, char **argv)
 	int i;
 	
 	i = argc - 1;
-	while (argv[i])
+	while (argv[i] && i != 0)
 	{
 		ft_putstr(argv[i]);
 		write (1, "\n", 1);
