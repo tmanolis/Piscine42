a6628ae4c4d691f058a644b3c0244aba69a69c3d
Tiffany MANOLIS <tmanolis@e3r4p12.42.fr>
2021-02-18 11:44:00 +0100

    ex03

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmanolis <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/18 09:33:58 by tmanolis          #+#    #+#             */
+/*   Updated: 2021/02/18 11:43:21 by tmanolis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int		main(int argc, char **argv)
+{
+	int		i;
+	char	*cache;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+		{
+			cache = argv[i];
+			argv[i] = argv[i + 1];
+			argv[i + 1] = cache;
+			i = 0;
+		}
+		i++;
+	}
+	i = 1;
+	while (argv[i])
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
