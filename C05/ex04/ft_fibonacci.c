b5b58a1540df4e6bb6efdd4d67cedaca83e4b734
Tiffany Manolis <tiffany.mano@gmail.com>
2021-02-19 21:55:28 +0100

    ex04

---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,15 @@
+int ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (- 1);
+	else if (index == 0)
+		return (0);
+	else if (index == 1)
+		return (1);
+	else if (index == 2)
+		return (1);
+	else if (index == 3)
+		return (2);
+	else
+		return(ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
