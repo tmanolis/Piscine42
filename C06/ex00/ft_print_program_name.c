e24dd0ed588611c12e17714f43fc27ac8ec4c9cd
Tiffany MANOLIS <tmanolis@e3r4p12.42.fr>
2021-02-20 10:30:56 +0100

    maj

--- C06/ex00/ft_print_program_name.c
+++ C06/ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: tmanolis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 10:06:05 by tmanolis          #+#    #+#             */
-/*   Updated: 2021/02/19 17:17:19 by tmanolis         ###   ########.fr       */
+/*   Updated: 2021/02/20 10:28:21 by tmanolis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	ft_putstr(char *str)
 
 int		main(int argc, char **argv)
 {
-	ft_putstr(argv[0]);
+	if (argc != 0)
+		ft_putstr(argv[0]);
 	return (0);
 }
