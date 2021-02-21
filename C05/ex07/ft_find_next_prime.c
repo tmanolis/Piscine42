2b9ff04427a97f7b35a13a28a706a8e609e4e809
Tiffany MANOLIS <tmanolis@e3r4p12.42.fr>
2021-02-21 17:26:37 +0100

    a finir

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,27 @@
+int	ft_is_prime(int nb)
+{
+	int i;
+
+	i = 2;
+	if (nb < 2)
+		return (0);
+	else if (nb == 2)
+		return (1);
+	else if (nb % 2 == 0)
+		return (0);
+	while (i * i < nb && i < 46341)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int ft_find_next_prime(int nb)
+{
+	if (ft_is prime(nb) == 1)
+		return (nb);
+	else
+		return
+}
