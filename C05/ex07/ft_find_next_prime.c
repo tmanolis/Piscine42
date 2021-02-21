1af42f9bb0fe178f1e1661fab7e59dd6bae7c981
Tiffany Manolis <tiffany.mano@gmail.com>
2021-02-21 21:55:08 +0100

    ca marche pas pour 9

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -1,27 +1,43 @@
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
 int	ft_is_prime(int nb)
 {
 	int i;
 
 	i = 2;
-	if (nb < 2)
+	if (nb < 2 || (nb % 2 == 0))
+	{
+		ft_putstr("la");
 		return (0);
+	}
 	else if (nb == 2)
 		return (1);
-	else if (nb % 2 == 0)
-		return (0);
 	while (i * i < nb && i < 46341)
 	{
 		if (nb % i == 0)
+		{
+			ft_putstr("ici");
 			return (0);
+		}
 		i++;
 	}
 	return (1);
 }
 
-int ft_find_next_prime(int nb)
+int	ft_find_next_prime(int nb)
 {
-	if (ft_is prime(nb) == 1)
+	if (nb > 2147483647)
+		return (0);
+	else if (ft_is_prime(nb) == 1)
 		return (nb);
-	else
-		return
+	return (ft_find_next_prime(nb + 1));
 }
---
+++ C05/ex07/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int	ft_find_next_prime(int nb);
+
+int	main(void)
+{
+	printf("%d", ft_find_next_prime(8));
+	return (0);
+}
