01e03358c3cbf5808c36cb57eb5a3c1db84910fa
Tiffany Manolis <tiffany.mano@gmail.com>
2021-02-23 22:47:16 +0100

    C08

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,12 @@
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+#include <unistd.h>
+typedef int t_bool;
+# define EVEN(x) (x%2==0)
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments."
+# define ODD_MSG "I have an odd number of arguments."
+# define SUCCESS 0
+
+#endif 
